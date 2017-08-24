6a2d0a43e24a7da5dfb006582b5e4543a1bf3a14
RVSW317029DT <RVSW317029DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-24 11:10:56 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot34TrialApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot34TrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot34TrialApplication.class, args);
+	}
+}
